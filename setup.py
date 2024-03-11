691d42bc22e325fa9f1b28ba34747ca9aa81fe9e
Hem Chandra <hem.chandra@corestack.io>
2024-03-11 05:43:56 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project is about Advanced RAG LLM Built on specific Medicat Abstract Corpus togther with ragas for validation of model and HyDE Integration',
+    author='Hem Chandra Padhalni',
+    license='',
+)
